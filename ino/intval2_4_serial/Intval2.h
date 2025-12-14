cf76ce3284474c1689e7f100227cefbb619e45b0
mattmcw <matt@sixteenmillimeter.com>
2025-12-14 06:25:45 -0800

    Remove output vars for now, unneeded. Idea is to implement Output() to not use delays but there could be some problems especially if there are overlapping outputs. Not going to cross that bridge at this time

--- ino/intval2_4_serial/Intval2.h
+++ ino/intval2_4_serial/Intval2.h
@@ -65,13 +65,6 @@ class Intval2 {
 	volatile unsigned long button_last[4] = {0, 0, 0, 0};
 	volatile unsigned long button_time = 0;
 
-	//OUTPUT VAR
-	volatile boolean output = false;
-	volatile boolean output_blink = false;
-	volatile uint16_t output_ms = 0;
-	volatile unsigned long output_time = 0;
-	volatile uint8_t output_count = 0;
-	volatile uint8_t output_i = 0;
 
 	//TIME VAR
 	volatile unsigned long timer;
